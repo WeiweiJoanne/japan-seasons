7aea694146d45ad7a47260e52058ccd6fe5eb938
Joanne Wei <joannewei0921@gmail.com>
2024-10-31 19:59:37 +0800

    update: 登入功能

--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -2,7 +2,7 @@ let isLogin = false
 const logBtn = document.querySelector('.log-in')
 const userStatus = document.querySelector('.user-status')
 
-document.addEventListener('click', () => {
+logBtn.addEventListener('click', () => {
     if (isLogin) return
     userStatus.classList.add('is-user-logged-in')
     isLogin = !isLogin
